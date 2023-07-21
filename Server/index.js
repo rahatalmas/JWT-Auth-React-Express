db151abafa710504fd72b192a532d86bc6fc5fee
rahatalmas <rahat.almas5599@gmail.com>
2023-07-21 18:41:49 +0600

    middlewareCheck

--- Server/index.js
+++ Server/index.js
@@ -3,11 +3,19 @@ const app = express();
 const PORT = 5000;
 const blogRoutes = require('./routes/blogRoutes');
 
+
 app.get("/",(req,res,next)=>{
     res.json({name:"Giant Server"});
 })
 
-app.use("/api",blogRoutes);
+app.use("/api",(req,res,next)=>{
+    console.log('blog middleware');
+    next();
+},blogRoutes);
+
+app.get("/test",(req,res,next)=>{
+     res.json({message:"test"});
+})
 
 app.listen(PORT,()=>{
     console.log(`server is running on port : ${PORT}`);
